0619e7d3c8363c15afd0fef1e8c75cf13d18cc27
Alejandro Rodriguez <alejandrorodriguezq0@gmail.com>
2025-08-16 11:53:09 -0500

    sec 05 race conditions

---
+++ src/main/java/com/training/sec05/Lec01RaceCondition.java
@@ -0,0 +1,61 @@
+package com.training.sec05;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Lec01RaceCondition {
+    private static final Logger log = LoggerFactory.getLogger(Lec01RaceCondition.class);
+
+    public static void main(String[] args) throws InterruptedException {
+        InventoryCounter inventoryCounter = new InventoryCounter();
+        IncrementingThread incrementingThread = new IncrementingThread(inventoryCounter);
+        DecrementingThread decrementingThread = new DecrementingThread(inventoryCounter);
+
+        incrementingThread.start();
+//        incrementingThread.join();;
+        decrementingThread.start();
+
+        incrementingThread.join();
+        decrementingThread.join();
+
+        log.info("We currently have {} items", inventoryCounter.getItems());
+    }
+
+    public static class DecrementingThread extends Thread {
+        private final InventoryCounter inventoryCounter;
+
+        public DecrementingThread(InventoryCounter inventoryCounter) {
+            this.inventoryCounter = inventoryCounter;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10000; i++) {
+                inventoryCounter.decrement();
+            }
+        }
+    }
+
+    public static class IncrementingThread extends Thread {
+        private final InventoryCounter inventoryCounter;
+
+        public IncrementingThread(InventoryCounter inventoryCounter) {
+            this.inventoryCounter = inventoryCounter;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10000; i++) {
+                inventoryCounter.increment();
+            }
+        }
+    }
+
+    private static class InventoryCounter {
+        private int items = 0;
+
+        public void increment() { items++; }
+        public void decrement() { items--; }
+        public int getItems() { return items; }
+    }
+}
